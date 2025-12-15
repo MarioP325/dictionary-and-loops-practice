bfe09dd171f4b68781f54c98eb255c76cabcda05
MarioP325 <mapantoja1@cps.edu>
2025-12-15 20:48:12 +0000

    dictionary

--- day_1_activities/2_main.py
+++ day_1_activities/2_main.py
@@ -44,9 +44,20 @@ import student_data
 # print(student_data.students)
 students = student_data.students
 print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+# print(students[0]['Email'][1])
+# print(students[0]['FName'])
+# print(students[0]['LName'])
+# print(students[0]['GL'])
+# print(students[1]['Email'][0])
+# print(students[1]['CPSID'])
+# print(students[1]['GL'])
+# print(students[1]['HR'])
+# print(students[35]['CPSID'])
+# print(students[35]['Email'][1])
+# print(students[35]['GL'])
+# print(students[35]['HR'])
 
 # What is being counted?
 
@@ -68,12 +79,21 @@ print(students[0]['Email'][1])
 #and printing the name and email of the students
 #we are also printing a line of underscores to separate the students
 #we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
-
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1])
+#     print(student['CPSID'])
+#     print(student['HR'])
+#     print(student['GL'])
+#     if student["GL"] >= 10:
+#         print("you can go to off campus")
+#         if student["LName"][0] == "R":
+#             print(student['Combo,Name'])
+#             print(student['HR'])
+#             if student["LName"][0] == "L":
+#                 break
+# print("_"*25)
 # What does the loop variable student represent on each pass?
 
 # How many times will this loop run?
